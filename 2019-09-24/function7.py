c2c115ed9693734f8a97726b0aa9d13e19595d4e
Fauzan Ardhana <31564867+fauzanardh@users.noreply.github.com>
2019-09-25 15:31:46 +0700

    making it to accept thousands

--- 2019-09-24/function7.py
+++ 2019-09-24/function7.py
@@ -2,8 +2,11 @@
 
 def romanArabic(arg):
     if type(arg) == int:
-        conv = [[10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']] # different arragement
-        result = ""
+        conv = [[1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'],
+                [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'],
+                [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'],
+                [1, 'I']] # different arragement
+        result = ''
         i = 0
         while arg > 0:
             while conv[i][0] > arg: i+=1
@@ -12,18 +15,21 @@ def romanArabic(arg):
         print(result)
     elif type(arg) == str:
         arg = arg.upper()
-        conv = [[9, 'IX'], [4, 'IV'], [10, 'X'], [5, 'V'], [1, 'I']] # different arragement
+        conv = [[900, 'CM'], [400, 'CD'], [90, 'XC'], [40, 'XL'],
+                [9, 'IX'], [4, 'IV'], [1000, 'M'], [500, 'D'],
+                [100, 'C'], [50, 'L'], [10, 'X'], [5, 'V'],
+                [1, 'I']] # different arragement
         result = 0
         i = 0
-        while arg != "":
+        while arg != '':
             if conv[i][1] in arg:
                 result += conv[i][0]
-                arg = arg.replace(conv[i][1], "", 1)
+                arg = arg.replace(conv[i][1], '', 1)
             else:
                 i += 1
         print(result)
     else:
-        print("Type not supported")
+        print('Type not supported')
 
-romanArabic("XIX")
-romanArabic(19)
+romanArabic('MCMXXVIII')
+romanArabic(1938)
