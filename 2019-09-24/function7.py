48e6b1a3835c134cb5f5d3c1790920d61e6f6c99
Fauzan Ardhana <31564867+fauzanardh@users.noreply.github.com>
2019-09-25 08:33:53 +0700

    making it less convoluted with less loop

--- 2019-09-24/function7.py
+++ 2019-09-24/function7.py
@@ -2,9 +2,9 @@
 
 def romanArabic(arg):
     if type(arg) == int:
+        conv = [[10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']] # different arragement
         result = ""
         i = 0
-        conv = [[10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']]
         while arg > 0:
             while conv[i][0] > arg: i+=1
             result += conv[i][1]
@@ -12,23 +12,16 @@ def romanArabic(arg):
         print(result)
     elif type(arg) == str:
         arg = arg.upper()
-        count = 0
-        while "IV" in arg:
-            count += 4
-            arg = arg.replace("IV", "", 1)
-        while "IX" in arg:
-            count += 9
-            arg = arg.replace("IX", "", 1)
-        while "I" in arg:
-            count += 1
-            arg = arg.replace("I", "", 1)
-        while "V" in arg:
-            count += 5
-            arg = arg.replace("V", "", 1)
-        while "X" in arg:
-            count += 10
-            arg = arg.replace("X", "", 1)
-        print(count)
+        conv = [[9, 'IX'], [4, 'IV'], [10, 'X'], [5, 'V'], [1, 'I']] # different arragement
+        result = 0
+        i = 0
+        while arg != "":
+            if conv[i][1] in arg:
+                result += conv[i][0]
+                arg = arg.replace(conv[i][1], "", 1)
+            else:
+                i += 1
+        print(result)
     else:
         print("Type not supported")
 
