e0cae99256abad6df0bc0628d08a5103da077276
Fauzan Ardhana <31564867+fauzanardh@users.noreply.github.com>
2019-10-02 20:51:57 +0700

    make it longer bois!

--- Mr Jude/FIle IO/e4_longest_series.py
+++ Mr Jude/FIle IO/e4_longest_series.py
@@ -1,38 +1,52 @@
 import re
 
 
-def indexOfNameStartsWith(lastChar, nameslist):
-    for index in range(len(nameslist)):
-        if nameslist[index].startswith(lastChar):
-            return index
-    return -1
+nsolutions = 0
+
+
+def search(sequences, ord_minc, curr_word, current_path, current_path_len, longest_path):
+    global nsolutions
+
+    current_path[current_path_len] = curr_word
+    current_path_len += 1
+
+    if current_path_len == len(longest_path):
+        nsolutions += 1
+    elif current_path_len > len(longest_path):
+        nsolutions = 1
+        longest_path[:] = current_path[:current_path_len]
+
+    last_char_index = ord(curr_word[-1]) - ord_minc
+    if last_char_index >= 0 and last_char_index < len(sequences):
+        for pair in sequences[last_char_index]:
+            if not pair[1]:
+                pair[1] = True
+                search(sequences, ord_minc, pair[0], current_path,
+                       current_path_len, longest_path)
+                pair[1] = False
 
 
 def findLongestSeries(filename):
     with open(filename, "r") as handler:
         names = re.findall("[a-z]+", handler.read())
-    longestSeries = []
-    currentSeries = []
 
+    ord_minc = ord(min(name[0] for name in names))
+    ord_maxc = ord(max(name[0] for name in names))
+    sequences = [[] for _ in range(ord_maxc - ord_minc + 1)]
     for name in names:
-        cName = name
-        currentSeries.append(cName)
+        sequences[ord(name[0]) - ord_minc].append([name, False])
 
-        nameslist = names[:]
-        nameslist.pop(nameslist.index(cName))
-        index = indexOfNameStartsWith(cName[-1], nameslist)
-        while index != -1:
-            cName = nameslist[index]
-            currentSeries.append(cName)
-            nameslist.pop(index)
-            index = indexOfNameStartsWith(cName[-1], nameslist)
+    current_path = [None] * len(names)
+    longest_path = []
 
-        if len(longestSeries) < len(currentSeries):
-            longestSeries = currentSeries
+    for seq in sequences:
+        for pair in seq:
+            pair[1] = True
+            search(sequences, ord_minc, pair[0],
+                   current_path, 0, longest_path)
+            pair[1] = False
 
-        currentSeries = []
-
-    return longestSeries
+    return longest_path
 
 
 print(findLongestSeries("pokemons.txt"))
