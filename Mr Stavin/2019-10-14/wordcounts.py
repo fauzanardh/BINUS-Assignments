70e356df7ffbcc575a3bd480739b8b90a80a5fed
Fauzan Ardhana <31564867+fauzanardh@users.noreply.github.com>
2019-10-15 08:48:45 +0700

    assignments?

---
+++ Mr Stavin/2019-10-14/wordcounts.py
@@ -0,0 +1,26 @@
+import re
+import pygal
+
+
+def makeBar(dataDict):
+    chart = pygal.Bar()
+    chart.title = "Words count in certain literature"
+    for key, value in dataDict.items():
+        chart.add(key, value)
+    chart.render_in_browser()
+
+
+def countWords(userInput):
+    counts = {}
+    for word in re.findall("[a-z]+", userInput.lower()):
+        counts[word] = counts.get(word, 0) + 1
+    return dict(sorted(counts.items(), key=lambda x: x[1])[::-1])
+
+
+def getTextFromFile(filename):
+    with open(filename, "r") as fp:
+        text = fp.read()
+    return text
+
+
+makeBar(countWords(getTextFromFile("pride_and_prejudice.txt")))
